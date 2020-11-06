5f85deae8b12c76cceda4b79614f3744114923f4
Roy Duineveld <royduineveld@gmail.com>
2020-11-06 10:20:08 +0100

    Init

---
+++ src/ImageResizerExtensionServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rapidez\ImageResizer;
+
+use Rapidez\ImageResizer\Controllers\ImageController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class ImageResizerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Route::middleware('web')->group(function () {
+            Route::get('image/{size}/{file}', ImageController::class)->where('file', '.*');
+        });
+
+        $this->mergeConfigFrom(__DIR__.'/config/imageresizer.php', 'imageresizer');
+
+        $this->publishes([
+            __DIR__.'/config/imageresizer.php' => config_path('imageresizer.php'),
+        ], 'config');
+    }
+}
---
+++ src/config/imageresizer.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'sizes' => [
+        '100x100', // Cart
+        '467',     // Product
+        '200',     // Category
+    ]
+];
