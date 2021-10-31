8bdbc1a310f8f691b32110feac983acb706920c9
Bob <bob@justbetter.nl>
2021-10-31 16:38:02 +0100

    Added the ability to include the watermark in images

--- config/imageresizer.php
+++ config/imageresizer.php
@@ -6,4 +6,15 @@ return [
         '400',     // Product
         '200',     // Category
     ],
+    'watermarks' => [
+        'positions' => [
+            'stretch' => Spatie\Image\Manipulations::FIT_STRETCH,
+            'tile' => Spatie\Image\Manipulations::FIT_CROP,
+            'top-left' => Spatie\Image\Manipulations::POSITION_TOP_LEFT,
+            'top-right' => Spatie\Image\Manipulations::POSITION_TOP_RIGHT,
+            'bottom-left' => Spatie\Image\Manipulations::POSITION_BOTTOM_LEFT,
+            'bottom-right' => Spatie\Image\Manipulations::POSITION_BOTTOM_RIGHT,
+            'center' => Spatie\Image\Manipulations::POSITION_CENTER
+        ]
+    ]
 ];
