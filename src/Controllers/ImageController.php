c359f777e4ca606fd8e9b1b1484d2c38177d31cf
Roy Duineveld <royduineveld@gmail.com>
2022-07-20 14:18:03 +0200

    Abort instead of custom UnreachableUrl exception

--- src/Controllers/ImageController.php
+++ src/Controllers/ImageController.php
@@ -7,7 +7,6 @@ use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Rapidez\Core\Models\Config;
-use Rapidez\ImageResizer\Exceptions\UnreachableUrl;
 use Spatie\Image\Image;
 use Spatie\Image\Manipulations;
 
@@ -92,7 +91,7 @@ class ImageController extends Controller
     public function saveTempFile($path)
     {
         if (!$stream = @fopen($path, 'r')) {
-            throw UnreachableUrl::create($path);
+            abort(404, "Url `{$url}` cannot be reached");
         }
 
         $temp = tempnam(sys_get_temp_dir(), 'rapidez');
