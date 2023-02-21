3620c1221493106c84d420b586269742b3cc5664
Roy Duineveld <royduineveld@gmail.com>
2023-02-21 13:12:21 +0100

    Homepage block sizes

--- config/imageresizer.php
+++ config/imageresizer.php
@@ -10,6 +10,8 @@ return [
         '80x80',   // Thumbs
         '400',     // Product
         '200',     // Category
+        '750',     // Homepage blocks
+        '1500',    // Homepage blocks
     ],
 
     'watermarks' => [
