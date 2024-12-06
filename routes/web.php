e370e91bc9cc4ecd388ac09a69c04fa1d480b9fe
indy koning <indy@justbetter.nl>
2024-12-06 11:49:55 +0100

    Add tip on disabling magento image hash generation

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Rapidez\Core\Facades\Rapidez;
+use Rapidez\Core\Http\Middleware\AuthenticateHealthCheck;
+use Rapidez\ImageResizer\Controllers\ImageController;
+
+const PATTERNS = [
+    'placeholder' => '[^\/]+',
+    'file'        => '.*\.((?!webp)[^\.])+',
+    'webp'        => '\.webp',
+];
+
+if (config('rapidez.imageresizer.sku.enabled')) {
+    Route::get('storage/{store}/resizes/{size}/sku/{file}', [ImageController::class, 'redirectFromSku'])
+        ->name('resized-sku');
+}
+
+Route::get('storage/{store}/resizes/{size}/{placeholder}/{file}{webp?}', ImageController::class)
+    ->where([
+        'store' => '[0-9]*',
+        ...PATTERNS,
+    ])
+    ->name('resized-image');
+
+// Backwards compatibility step.
+Route::get('storage/resizes/{size}/{placeholder}/{file}{webp?}', function (string $size, string $placeholder, string $file, string $webp = '') {
+    return redirect(route('resized-image', ['store' => config('rapidez.store'), ...compact('size', 'placeholder', 'file', 'webp')]), 301);
+})->where(PATTERNS);
--- src/ImageResizerServiceProvider.php
+++ src/ImageResizerServiceProvider.php
@@ -2,19 +2,13 @@
 
 namespace Rapidez\ImageResizer;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Rapidez\ImageResizer\Commands\RemoveResizesCommand;
 use Rapidez\ImageResizer\Controllers\ImageController;
+use Rapidez\ImageResizer\Listeners\Healthcheck\ImageGenerationHealthcheck;
 
 class ImageResizerServiceProvider extends ServiceProvider
 {
-    const PATTERNS = [
-        'placeholder' => '[^\/]+',
-        'file'        => '.*\.((?!webp)[^\.])+',
-        'webp'        => '\.webp',
-    ];
-
     /**
      * Bootstrap services.
      *
@@ -22,31 +16,42 @@ class ImageResizerServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->bootConfig()
+            ->bootRoutes()
+            ->bootCommands()
+            ->bootListeners();
+    }
+
+    public function bootConfig(): self
+    {
         $this->mergeConfigFrom(__DIR__.'/../config/rapidez/imageresizer.php', 'rapidez.imageresizer');
-
-        if (config('rapidez.imageresizer.sku.enabled')) {
-            Route::get('storage/{store}/resizes/{size}/sku/{file}', [ImageController::class, 'redirectFromSku'])
-                ->name('resized-sku');
-        }
-
-        Route::get('storage/{store}/resizes/{size}/{placeholder}/{file}{webp?}', ImageController::class)
-            ->where([
-                'store' => '[0-9]*',
-                ...self::PATTERNS,
-            ])
-            ->name('resized-image');
-
-        // Backwards compatibility step.
-        Route::get('storage/resizes/{size}/{placeholder}/{file}{webp?}', function (string $size, string $placeholder, string $file, string $webp = '') {
-            return redirect(route('resized-image', ['store' => config('rapidez.store'), ...compact('size', 'placeholder', 'file', 'webp')]), 301);
-        })->where(self::PATTERNS);
-
         $this->publishes([
             __DIR__.'/../config/rapidez/imageresizer.php' => config_path('rapidez/imageresizer.php'),
         ], 'config');
 
+        return $this;
+    }
+
+    public function bootRoutes(): self
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        return $this;
+    }
+
+    public function bootCommands(): self
+    {
         $this->commands([
             RemoveResizesCommand::class,
         ]);
+
+        return $this;
+    }
+
+    public function bootListeners(): self
+    {
+        ImageGenerationHealthcheck::register();
+
+        return $this;
     }
 }
