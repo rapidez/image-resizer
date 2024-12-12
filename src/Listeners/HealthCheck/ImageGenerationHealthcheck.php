4767e68daa06c20698edc7cd205fcb03c6585977
indykoning <15870933+indykoning@users.noreply.github.com>
2024-12-12 17:09:36 +0100

    Magento image hash generation healthcheck + moved routes (#37)

---
+++ src/Listeners/HealthCheck/ImageGenerationHealthcheck.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rapidez\ImageResizer\Listeners\Healthcheck;
+
+use Rapidez\Core\Facades\Rapidez;
+use Rapidez\Core\Listeners\Healthcheck\Base;
+
+class ImageGenerationHealthcheck extends Base
+{
+    public function handle()
+    {
+        $response = [
+            'healthy'  => true,
+            'messages' => [],
+        ];
+
+        $mediaUrlFormat = Rapidez::config('web/url/catalog_media_url_format', 'hash');
+        if ($mediaUrlFormat !== 'image_optimization_parameters') {
+            $response['messages'][] = [
+                'type'  => 'info',
+                'value' => __('Tip: setting the "Catalog media URL format" to "Image optimization based on query parameters" helps greatly reduce disk usage, and Rapidez works with that setting.'),
+            ];
+        }
+
+        return $response;
+    }
+}
